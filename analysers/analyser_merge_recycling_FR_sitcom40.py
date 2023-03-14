0a9953b38b3ca3284e2ea16da1c8a281eefd5b21
Vincent Bergeot <veille@numetlib.fr>
2023-03-14 09:14:17 +0100

    Create merge analyser to adrien CACL
    cela ne conserne que le verre, pas d'info autre que lat et lon dans le fichier csv
    
    je ne sais pas quoi faire des ref
    et peut-être ajouter un static sur operator=CACL

--- analysers/analyser_merge_recycling_FR_sitcom40.py
+++ analysers/analyser_merge_recycling_FR_sitcom40.py
@@ -24,44 +24,33 @@ from modules.OsmoseTranslation import T_
 from .Analyser_Merge import Analyser_Merge_Point, Source, CSV, Load_XY, Conflate, Select, Mapping
 
 
-class Analyser_Merge_Recycling_FR_sitcom40(Analyser_Merge_Point):
+class Analyser_Merge_Recycling_FR_CACL(Analyser_Merge_Point):
     def __init__(self, config, logger = None):
         Analyser_Merge_Point.__init__(self, config, logger)
         self.def_class_missing_official(item = 8120, id = 40, level = 3, tags = ['merge', 'recycling', 'fix:survey'],
-            title = T_('{0} recycling not integrated', 'SITCOM40'))
+            title = T_('{0} recycling not integrated', 'CACL'))
         self.def_class_possible_merge(item = 8121, id = 41, level = 3, tags = ['merge', 'recycling', 'fix:chair'],
-            title = T_('{0} recycling, integration suggestion', 'SITCOM40'))
+            title = T_('{0} recycling, integration suggestion', 'CACL'))
 
         self.init(
-            "https://www.sitcom40.fr/",
+            "https://data.cacl-guyane.fr/explorateur/donnees/environnement-positions-pav-verre",
             "Points d'apport volontaire",
-            CSV(Source(attribution = "SITCOM - Côte sud des Landes",
-                    fileUrl = "https://publicsitcom.z28.web.core.windows.net/OSM/ExportForOSM.csv"),
+            CSV(Source(attribution = "CACL - Côte sud des Landes",
+                    fileUrl = "https://api-data.cacl-guyane.fr/v1/environnement-positions-pav-verre?format=csv"),
                 separator = ";"),
-            Load_XY("LON", "LAT"),
+            Load_XY("lon", "lat"),
             Conflate(
                 select = Select(
                     types = ["nodes", "ways"],
                     tags = {"amenity": "recycling"}),
                 conflationDistance = 100,
-                osmRef = "ref:FR:SITCOM",
+                osmRef = "",
                 mapping = Mapping(
                     static1 = {
                         "amenity": "recycling",
                         "recycling_type": "container"},
+                        "recycling_glassbottles": "tes"
                     static2 = {"source": self.source},
                     mapping1 = {
-                        "operator": "operator",
-                        "ref:FR:SITCOM": "ref:FR:SITCOM",
-                        "recycling:plastic_bottles": lambda fields: "yes" if fields["recycling:plastic_bottles"] == "true" else None,
-                        "recycling:glass_bottles": lambda fields: "yes" if fields["recycling:glass_bottles"] == "true" else None,
-                        "recycling:beverage_cartons": lambda fields: "yes" if fields["recycling:beverage_cartons"] == "true" else None,
-                        "recycling:paper": lambda fields: "yes" if fields["recycling:paper"] == "true" else None,
-                        "recycling:newspaper": lambda fields: "yes" if fields["recycling:newspaper"] == "true" else None,
-                        "recycling:magazines": lambda fields: "yes" if fields["recycling:magazines"] == "true" else None,
-                        "recycling:paper_packaging": lambda fields: "yes" if fields["recycling:paper_packaging"] == "true" else None,
-                        "recycling:cans": lambda fields: "yes" if fields["recycling:cans"] == "true" else None,
-                        "recycling:batteries": lambda fields: "yes" if fields["recycling:batteries"] == "true" else None,
-                        "recycling:clothes": lambda fields: "yes" if fields["recycling:clothes"] == "true" else None,
-                        "recycling:waste": lambda fields: "yes" if fields["recycling:waste"] == "true" else None,},
+                        
                     text = lambda tags, fields: {"en": fields["name"]} )))
